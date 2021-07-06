38dad5ed237fbec0f11b044859e30a85d1798aa1
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-05 20:45:02 -0500

    modified controller class from oop to fop

--- src/controllers/exampleController.ts
+++ src/controllers/exampleController.ts
@@ -2,23 +2,37 @@ import { Request, Response } from 'express';
 
 import ExampleModel, { Example } from '../models/Example';
 
-// Encapsules the controller methods into one class
-class ExampleController {
 
-    public async index (req: Request, res: Response): Promise<void> {
-        const examples: Example[] = await ExampleModel.find();
-        res.json({examples});
-    }
+export const getExamples = async (req: Request, res: Response): Promise<void> => {
+    const examples: Example[] = await ExampleModel.find();
+    res.json({examples});
+};
 
-    public async saveExample(req: Request, res: Response): Promise<void>{
-        const { title, description } = req.body;
-        const example: Example = new ExampleModel({ title, description });
-        await example.save();
-        res.send('Example saved on database');
-    }
-
+export const saveExample = async (req: Request, res: Response): Promise<void> => {
+    const { title, description } = req.body;
+    const example: Example = new ExampleModel({ title, description });
+    await example.save();
+    res.send('Example saved on database');
 }
+
+
+// Encapsules the controller methods into one class
+// class ExampleController {
+
+//     public async index (req: Request, res: Response): Promise<void> {
+//         const examples: Example[] = await ExampleModel.find();
+//         res.json({examples});
+//     }
+
+//     public async saveExample(req: Request, res: Response): Promise<void>{
+//         const { title, description } = req.body;
+//         const example: Example = new ExampleModel({ title, description });
+//         await example.save();
+//         res.send('Example saved on database');
+//     }
+
+// }
 
 // we could define an object an define all methods inside, then, it could be exported
 
-export const exampleController = new ExampleController();
+// export const exampleController = new ExampleController();
--- src/routes/example.routes.ts
+++ src/routes/example.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 
-import { exampleController } from '../controllers/exampleController';
+// import { exampleController } from '../controllers/exampleController'; // if we use a class
+import { getExamples, saveExample } from '../controllers/exampleController';
 // Just a route for the api
 const router: Router = Router();
 // Example: the following route must be used ONLY to define RESTful APi routes: http://localhost:3000/example
 router.route('/')
-    .get(exampleController.index) // The function the method refferences is located on controllers folder
-    .post(exampleController.saveExample);
+    .get(getExamples) // The function the method refferences is located on controllers folder
+    .post(saveExample);
 
 export default router;
