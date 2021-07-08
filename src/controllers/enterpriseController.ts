b8e8a6d197ce31c2a9f067259ab55a5bb5de297e
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-08 05:26:04 -0500

    messages on save updated

--- src/controllers/enterpriseController.ts
+++ src/controllers/enterpriseController.ts
@@ -26,10 +26,16 @@ export const getEnterpriseById = async (req: Request, res: Response): Promise<vo
 export const saveEnterprise = async (req: Request, res: Response): Promise<void> => {
     const { name, acronym, access } = req.body;
     try {
+        const enterprises: Enterprise[] = await EnterpriseModel.find({'access.email': access.email});
+        if (enterprises.length > 0){
+            res.json({msg: 'El email se encuentra duplicado'}).status(500);
+            return;
+        }
+
         const enterprise: Enterprise = new EnterpriseModel({ name, acronym, access });
         await enterprise.save();
-        res.json({enterprise, msg: 'Enterprise saved on database'});
+        res.json({enterprise, msg: 'La empresa ha sido almacenada con éxito'});
     } catch (error) {
-        res.json({ error: error }).status(500);
+        res.json({ error: error , msg: 'La empresa ha sido almacenada con éxito'}).status(500);
     }
 }
