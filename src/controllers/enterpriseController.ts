d6c23f56e3cb68cbe656f69e7d22360c6cc6b18b
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-08 19:02:36 -0500

    email validation

--- src/controllers/enterpriseController.ts
+++ src/controllers/enterpriseController.ts
@@ -26,16 +26,25 @@ export const getEnterpriseById = async (req: Request, res: Response): Promise<vo
 export const saveEnterprise = async (req: Request, res: Response): Promise<void> => {
     const { name, acronym, access } = req.body;
     try {
-        const enterprises: Enterprise[] = await EnterpriseModel.find({'access.email': access.email});
-        if (enterprises.length > 0){
-            res.json({msg: 'El email se encuentra duplicado'}).status(500);
-            return;
-        }
-
         const enterprise: Enterprise = new EnterpriseModel({ name, acronym, access });
         await enterprise.save();
         res.json({enterprise, msg: 'La empresa ha sido almacenada con éxito'});
     } catch (error) {
-        res.json({ error: error , msg: 'La empresa ha sido almacenada con éxito'}).status(500);
+        res.json({ error: error , msg: 'Hubo un problema con el registro'}).status(500);
+    }
+}
+
+// Check wether the email is available or not
+export const isEmailUnique = async (req: Request, res: Response): Promise<void> => {
+    const email: String = req.params.email;
+    try {
+        const enterprises: Enterprise[] = await EnterpriseModel.find({'access.email': email});
+        if (enterprises.length > 0)
+            res.json({msg: 'El email se encuentra ocupado', val: false});
+        else
+            res.json({msg: 'Email disponible', val: true});
+        
+    } catch (error) {
+        res.json({ error: error , msg: 'Hubo un problema con la consulta'}).status(500);
     }
 }
--- src/routes/enterprise.routes.ts
+++ src/routes/enterprise.routes.ts
@@ -1,14 +1,17 @@
 import {Router} from 'express';
 
-import { getEnterprises, saveEnterprise, getEnterpriseById } from '../controllers/enterpriseController';
+import { getEnterprises, saveEnterprise, getEnterpriseById, isEmailUnique } from '../controllers/enterpriseController';
 
 const router: Router = Router();
 
 router.route('/')
     .get(getEnterprises) 
     .post(saveEnterprise);
-
+    
 router.route('/:id')
     .get(getEnterpriseById);
+    
+router.route('/email_validation/:email')
+    .get(isEmailUnique);
 
 export default router;
