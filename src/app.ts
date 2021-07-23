10958893df985f073d570b7e205b7b1fb8d5f0c4
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-22 22:02:53 -0500

    roles

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import passport from 'passport'
 
 // imported files
 
-import { enterpriseAuth, userAuth } from './middlewares/passport'
+import Authentication from './middlewares/passport'
 import enterpriseRoutes from './routes/enterprise.routes'
 import groupsRoutes from './routes/groups.routes';
 import userRoutes from './routes/user.routes';
@@ -35,8 +35,7 @@ app.use(cors());
 app.use(express.json()); // Uses JSON to send and retrieve data
 app.use(express.urlencoded({extended: false})); // Interpreter url requests
 app.use(passport.initialize()); // Uses passport
-passport.use(enterpriseAuth); // Passport uses the configuration from the middleware
-passport.use(userAuth); // Passport uses the configuration from the middleware
+passport.use(Authentication); // Passport uses the configuration from the middleware
 
 // Routes: routes from the api
 app.use('/enterprise', enterpriseRoutes);
--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,8 +1,9 @@
 import {Strategy, ExtractJwt, StrategyOptions} from 'passport-jwt';
 
 import config from '../config/config'
-import EnterpriseModel, { Enterprise } from '../models/Enterprise'
-import UserModel, { User } from '../models/User'
+import authorization from './roles';
+import { Enterprise } from '../models/Enterprise'
+import { User  } from '../models/User'
 
 const opts: StrategyOptions = {
     // Authorization: Bearer <token>
@@ -11,9 +12,10 @@ const opts: StrategyOptions = {
 }
 
 // The payload is the data object inside our jwt
-export const enterpriseAuth = new Strategy(opts, async (payload, done) => {
+const enterpriseAuth = new Strategy(opts, async (payload, done) => {
     try {
-        const enterprise:Enterprise | null = await EnterpriseModel.findById(payload.id);
+        const payloadModel = authorization[payload.type];
+        const enterprise:Enterprise | User | null = await payloadModel.findById(payload.id);
         // If user is matched, return the enterprise. Else, return a false
         // Null is for the error
         if (enterprise) {
@@ -25,17 +27,4 @@ export const enterpriseAuth = new Strategy(opts, async (payload, done) => {
     }
 });
 
-// The payload is the data object inside our jwt
-export const userAuth = new Strategy(opts, async (payload, done) => {
-    try {
-        const user:User | null = await UserModel.findById(payload.id);
-        // If user is matched, return the user. Else, return a false
-        // Null is for the error
-        if (user) {
-            return done(null, user);
-        }
-        return done(null, false)
-    } catch (error) {
-        console.log(error)
-    }
-});
+export default enterpriseAuth;
